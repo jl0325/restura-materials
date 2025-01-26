dcca4dd57b49ffbbbb54a524e3e9fa9e4662dd17
jl0325 <jalandazabal@gmail.com>
2025-01-27 10:29:14 +1100

    update navbar function instio js file

--- js/navbar.js
+++ js/navbar.js
@@ -7,6 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
         if (userData.admin === 1) {
             // Add admin-specific links to the navbar
             const adminLinks = `
+                <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
+                <li class="nav-item"><a href="admin-forms.html" class="nav-link">Forms</a></li>
                 <li class="nav-item"><a href="admin-user.html"class=" nav-link">Users</a></li>
                 <li class="nav-item"><a href="admin-materials.html" class="nav-link">Materials</a></li>
                 <li class="nav-item" id="logout-link"><a href="logout.html" class="nav-link">Logout</a></li>
@@ -14,6 +16,11 @@ document.addEventListener('DOMContentLoaded', () => {
             navbar.insertAdjacentHTML('beforeend', adminLinks);
         }
     } else {
-        console.warn('No user data found. Please ensure the user is logged in.');
+        const adminLinks = `
+            <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
+            <li class="nav-item"><a href="admin-forms.html" class="nav-link">Forms</a></li>
+            <li class="nav-item" id="logout-link"><a href="logout.html" class="nav-link">Logout</a></li>
+        `;
+        navbar.insertAdjacentHTML('beforeend', adminLinks);
     }
 });
