3ae00a64e608a16775305dcf16c30f335352dc62
jl0325 <jalandazabal@gmail.com>
2025-01-27 11:16:47 +1100

    Update navbar style and function on mobile

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const navbar = document.getElementById('navbar');
+    const toggleButton = document.getElementById('navbar-toggle');
+    const navbarMenu = document.getElementById('navbar-menu');
     const currentUser = localStorage.getItem('currentUser');
+
+    // Toggle Navbar for mobile view
+    toggleButton.addEventListener('click', () => {
+        navbarMenu.classList.toggle('open');
+    });
 
     if (currentUser) {
         const userData = JSON.parse(currentUser);
@@ -9,18 +15,18 @@ document.addEventListener('DOMContentLoaded', () => {
             const adminLinks = `
                 <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
                 <li class="nav-item"><a href="admin-forms.html" class="nav-link">Forms</a></li>
-                <li class="nav-item"><a href="admin-user.html"class=" nav-link">Users</a></li>
+                <li class="nav-item"><a href="admin-user.html" class="nav-link">Users</a></li>
                 <li class="nav-item"><a href="admin-materials.html" class="nav-link">Materials</a></li>
                 <li class="nav-item" id="logout-link"><a href="logout.html" class="nav-link">Logout</a></li>
             `;
-            navbar.insertAdjacentHTML('beforeend', adminLinks);
+            navbarMenu.insertAdjacentHTML('beforeend', adminLinks);
         }
     } else {
-        const adminLinks = `
+        const defaultLinks = `
             <li class="nav-item"><a href="index.html" class="nav-link">Home</a></li>
             <li class="nav-item"><a href="admin-forms.html" class="nav-link">Forms</a></li>
             <li class="nav-item" id="logout-link"><a href="logout.html" class="nav-link">Logout</a></li>
         `;
-        navbar.insertAdjacentHTML('beforeend', adminLinks);
+        navbarMenu.insertAdjacentHTML('beforeend', defaultLinks);
     }
 });
